812173f196fc616d2927b453d8491cc3e3e7b324
Christopher D'Silva <chris.l.dsilva@gmail.com>
2018-06-29 21:52:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ function shout(string)
   {
     return string.toUpperCase()
   }
-  function whisper(string) 
+function whisper(string) 
   {
     return string.toLowerCase()
   }
-  function logShout(string) 
+function logShout(string) 
   {
     console.log(); string.toUpperCase()
   }
-  function logWhisper(string) 
+function logWhisper(string) 
   {
     console.log(); string.toUpperCase()
   }
-  function sayHiToGrandma()
+function sayHiToGrandma()
   {
     
   }
+  
