80b4eee06316e77e52fe2cacc85be3a1067f991a
Christopher D'Silva <chris.l.dsilva@gmail.com>
2018-06-29 21:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,3 @@ function shout(string)
   {
     return string.toLowerCase()
   }
-  function logShout(string) 
-  {
-    return string.toUpperCase()
-  }
-  function logWhisper(string) 
-  {
-    return string.toUpperCase()
-  }
-  function logWhisper
